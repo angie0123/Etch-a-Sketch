aac3d935e0f2207f86cef396d4b0ac35f3830310
Angela Sun <angelasun1995@gmail.com>
2022-03-12 17:42:57 -0500

    Add 16 x 16 grid in js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("connected");
+const body = document.querySelector("body");
+const gridContainer = document.createElement("div");
+gridContainer.classList.add("grid-container");
+body.appendChild(gridContainer);
+//1. create 16 * 16 square divs
+
+// Will fill 16 columns with 16 squares each , as divs stack on top
+
+for (let i = 0; i < 16; i++) {
+  const columnContainer = document.createElement("div");
+  columnContainer.classList.add("column");
+  for (let j = 0; j < 16; j++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    columnContainer.appendChild(square);
+  }
+  gridContainer.appendChild(columnContainer);
+}
