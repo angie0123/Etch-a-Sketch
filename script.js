1acec5d6b36709b5b4a2ba36f74eab95db514cb6
Angela Sun <angelasun1995@gmail.com>
2022-03-12 19:07:55 -0500

    Add height and width to squares

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const body = document.querySelector("body");
+const GRID_HEIGHT = 500;
+const GRID_WIDTH = 500;
+const squaresPerColumn = 16;
 const gridContainer = document.createElement("div");
 gridContainer.classList.add("grid-container");
+const squareLength = Math.floor(GRID_HEIGHT / squaresPerColumn);
 body.appendChild(gridContainer);
 //1. create 16 * 16 square divs
 
@@ -9,10 +13,14 @@ body.appendChild(gridContainer);
 for (let i = 0; i < 16; i++) {
   const columnContainer = document.createElement("div");
   columnContainer.classList.add("column");
+  columnContainer.style.width = `${squareLength}px`;
   for (let j = 0; j < 16; j++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.height = `${squareLength}px`;
     columnContainer.appendChild(square);
   }
+
+  //   columnContainer.style.width = unitHeight;
   gridContainer.appendChild(columnContainer);
 }
