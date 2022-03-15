ab39bc285f2be99943a9513eee9eb852500ca0b2
Angela Sun <angelasun1995@gmail.com>
2022-03-14 20:42:57 -0400

    Add slider to customize grid density in script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const body = document.querySelector("body");
 const GRID_HEIGHT = 500;
 const GRID_WIDTH = 500;
-const squaresPerColumn = 16;
-const squareLength = Math.floor(GRID_HEIGHT / squaresPerColumn);
 
 // Will fill 16 columns with 16 squares each , as divs stack on top
-function initGrid() {
+function initGrid(squaresPerSide) {
+  let squareLength = Math.floor(GRID_HEIGHT / squaresPerSide);
   const gridContainer = document.createElement("div");
   gridContainer.classList.add("grid-container");
   body.appendChild(gridContainer);
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < squaresPerSide; i++) {
     const columnContainer = document.createElement("div");
     columnContainer.classList.add("column");
     columnContainer.style.width = `${squareLength}px`;
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < squaresPerSide; j++) {
       const square = document.createElement("div");
       square.classList.add("square");
       square.style.height = `${squareLength}px`;
@@ -34,13 +33,31 @@ function initMenu() {
   clearBtn.textContent = "Clear";
   clearBtn.addEventListener("click", clearGrid);
   body.appendChild(clearBtn);
+
+  const slider = document.createElement("input");
+  slider.type = "range";
+  slider.min = 1;
+  slider.max = 100;
+  slider.value = 16;
+  slider.classList.add("slider");
+  const sliderValueMessage = document.createElement("div");
+  sliderValueMessage.textContent = `${slider.value} x ${slider.value}`;
+
+  body.appendChild(slider);
+  body.appendChild(sliderValueMessage);
+
+  slider.addEventListener("input", () => {
+    squaresPerSide = slider.value;
+    sliderValueMessage.textContent = `${slider.value} x ${slider.value}`;
+    clearGrid(squaresPerSide);
+  });
 }
 
-function clearGrid() {
+function clearGrid(squaresPerSide) {
   const gridContainer = document.querySelector("div.grid-container");
   gridContainer.remove();
-  initGrid();
+  initGrid(squaresPerSide);
 }
 
 initMenu();
-initGrid();
+initGrid(100);
